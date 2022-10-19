5c58ff5192839b9cfc751ba2dcf06b8563e67f79
Matias Sciancalepore <msciancalepore47@gmail.com>
2022-10-19 11:03:50 -0300

    se implemento el router con el switch case,listo para ser completado

--- route.php
+++ route.php
@@ -1,1 +1,20 @@
-<?php
+<?php 
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$action= "home";
+if(!empty($_REQUEST['action']))
+{
+    $action= $_REQUEST['action'];
+}
+
+$params= explode("/", $action);
+switch ($params[0]){
+    case '':
+        ;
+        break;
+
+    default:
+      echo "error";
+      break;
+} 
