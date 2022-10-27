a9d8df04b70d9b89f506b6ece523e25c19226017
Tobias Lettos <lettos.tobias02@gmail.com>
2022-10-27 17:00:26 -0300

    ✔agrego funcionalidad a los botones de agregar numero y operaciones

--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
 const display = new Display(displayValorAnterior , displayValorActual);
+
+  botonesNumeros.forEach(boton =>{
+        boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+
+  });  
+  
+  botonesOperadores.forEach(boton =>{
+        boton.addEventListener('click', () => display.computar(boton.value));
+
+  });  
