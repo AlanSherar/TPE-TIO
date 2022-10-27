7ef7582fbe8edc83c49c0328a2ff8940b56c4b86
Tobias Lettos <lettos.tobias02@gmail.com>
2022-10-27 17:16:49 -0300

    agrego la funcion  computar

--- js/display.js
+++ js/display.js
@@ -1,1 +1,11 @@
-aaa
+"use strict"
+
+
+function computar(tipo){
+
+    this.tipoOperacion !=='igual' && this.calcular();
+    this.tipoOperacion = tipo ;
+    this.valorAnterior = this.valorActual || this.valorAnterior;
+    this.valorActual = '';
+    this.imprimirValores();
+}
