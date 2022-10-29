1e0d94e0dbc62ead80d495e02352b3bce8aa7c4b
Lautaro Scuffi <scuffilautaro@gmail.com>
2022-10-29 10:22:13 -0300

    resuelve conflictos y agrega metodos

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,10 +2,7 @@
 class Calculadora{
      Restar(numero1 , numero2){
     return (numero1 - numero2) ;
-<<<<<<<<< Temporary merge branch 1
+
 }
 
-aaassdadaddas
-=========
 }
->>>>>>>>> Temporary merge branch 2
