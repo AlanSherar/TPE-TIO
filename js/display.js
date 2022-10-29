e6216273b142f4c767de67208a909c7b7d610b4e
Lautaro Scuffi <scuffilautaro@gmail.com>
2022-10-29 10:12:29 -0300

    agrega metodos para imprimir y borrar

--- js/display.js
+++ js/display.js
@@ -15,11 +15,20 @@ class Display{
     this.valorActual = '';
     this.imprimirValores();
 }
+borrar(){
+    this.valorActual = this.valorActual.toString().slice(0,-1) ;
+    this.imprimirValores() ;
+}
 
  agregarNumero(numero){
 
    if(numero == '.' && this.valorActual.includes('.'))return
    this.valorActual = this.valorActual.toString() + numero.toString();
    this.imprimirValores(); 
+}
+imprimirValores(){
+    this.displayValorActual.textContent = this.valorActual ;
+    this.displayValorAnterior.textContent = this.valorAnterior ;
+
 }
 }
