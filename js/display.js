1290b5474a926e6c65ce3167fbcecfb17839aa08
Tobias Lettos <lettos.tobias02@gmail.com>
2022-10-31 16:59:58 -0300

    tabulacion archivo display.js

--- js/display.js
+++ js/display.js
@@ -1,5 +1,8 @@
 "use strict"
 class Display{
+
+
+
     constructor(displayValorAnterior , displayValorActual){
         this.displayValorAnterior = displayValorAnterior;
         this.valorActual = displayValorActual;
@@ -7,28 +10,35 @@ class Display{
         this.valorAnterior = '' ;
         this.valorActual = '' ;
     }
+
+
+
      computar(tipo){
 
-    this.tipoOperacion !=='igual' && this.calcular();
-    this.tipoOperacion = tipo ;
-    this.valorAnterior = this.valorActual || this.valorAnterior;
-    this.valorActual = '';
-    this.imprimirValores();
-}
-borrar(){
-    this.valorActual = this.valorActual.toString().slice(0,-1) ;
-    this.imprimirValores() ;
-}
+        this.tipoOperacion !=='igual' && this.calcular();
+        this.tipoOperacion = tipo ;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
 
- agregarNumero(numero){
 
-   if(numero == '.' && this.valorActual.includes('.'))return
-   this.valorActual = this.valorActual.toString() + numero.toString();
-   this.imprimirValores(); 
-}
-imprimirValores(){
-    this.displayValorActual.textContent = this.valorActual ;
-    this.displayValorAnterior.textContent = this.valorAnterior ;
+    borrar(){
+        
+        this.valorActual = this.valorActual.toString().slice(0,-1) ;
+        this.imprimirValores() ;
+    }
 
-}
+    agregarNumero(numero){
+
+        if(numero == '.' && this.valorActual.includes('.'))return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores(); 
+    }
+    imprimirValores(){
+
+        this.displayValorActual.textContent = this.valorActual ;
+        this.displayValorAnterior.textContent = this.valorAnterior ;
+
+    }
 }
