eada6bb5a071ce4fc18549ff543d63d37d149340
Matias Sciancalepore <msciancalepore47@gmail.com>
2022-11-02 12:40:47 -0300

    agrego funciones sumar,multiplicar y dividir

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,7 +2,15 @@
 class Calculadora{
      Restar(numero1 , numero2){
     return (numero1 - numero2) ;
-
-}
+    }
+    sumar(numero1 , numero2){
+        return (numero1 + numero2) ;
+        }
+    multiplicar(numero1 , numero2){
+        return (numero1 * numero2) ;
+        }
+    dividir(numero1 , numero2){
+        return (numero1 / numero2) ;
+        }
 
 }
