e81dade921bd253d0b3ead78a411772ab3052b47
Lautaro Scuffi <scuffilautaro@gmail.com>
2022-11-02 11:13:27 -0300

    nada

--- js/display.js
+++ js/display.js
@@ -1,34 +1,36 @@
-"use strict"
-class Display{
-    constructor(displayValorAnterior , displayValorActual){
-        this.displayValorAnterior = displayValorAnterior;
-        this.valorActual = displayValorActual;
-        this.calculadora = new Calculadora() ;
-        this.valorAnterior = '' ;
-        this.valorActual = '' ;
+    "use strict"
+    class Display{
+        constructor(displayValorAnterior , displayValorActual){
+            this.displayValorAnterior = displayValorAnterior;
+            this.valorActual = displayValorActual;
+            this.calculadora = new Calculadora() ;
+            this.valorAnterior = '' ;
+            this.valorActual = '' ;
+        }
+        computar(tipo){
+
+        this.tipoOperacion !=='igual' && this.calcular();
+        this.tipoOperacion = tipo ;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
     }
-     computar(tipo){
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0,-1) ;
+        this.imprimirValores() ;
+    }
+    
 
-    this.tipoOperacion !=='igual' && this.calcular();
-    this.tipoOperacion = tipo ;
-    this.valorAnterior = this.valorActual || this.valorAnterior;
-    this.valorActual = '';
-    this.imprimirValores();
-}
-borrar(){
-    this.valorActual = this.valorActual.toString().slice(0,-1) ;
-    this.imprimirValores() ;
-}
+    agregarNumero(numero){
 
- agregarNumero(numero){
+    if(numero == '.' && this.valorActual.includes('.'))return
+    this.valorActual = this.valorActual.toString() + numero.toString();
+    this.imprimirValores(); 
+    }
+    imprimirValores(){
+        this.displayValorActual.textContent = this.valorActual ;
+        this.displayValorAnterior.textContent = this.valorAnterior ;
 
-   if(numero == '.' && this.valorActual.includes('.'))return
-   this.valorActual = this.valorActual.toString() + numero.toString();
-   this.imprimirValores(); 
-}
-imprimirValores(){
-    this.displayValorActual.textContent = this.valorActual ;
-    this.displayValorAnterior.textContent = this.valorAnterior ;
-
-}
-}
+    }
+    
+    }
